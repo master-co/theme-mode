831696a6143c42de64117fdaa404a9bd039e907b
Aron <i@aron.tw>
2024-03-01 23:29:57 +0800

    Add ThemeModeProvider component and update index.ts and vite.config.ts

---
+++ packages/vue/src/ThemeModeProvider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import ThemeMode, { type Options } from 'theme-mode'
+import { onMounted, provide, shallowReactive, onUnmounted } from 'vue'
+
+const props = defineProps<{ host?: HTMLElement, options?: Options }>()
+const themeMode = shallowReactive<ThemeMode>(new ThemeMode(props.options, props.host))
+
+onMounted(() => themeMode.init())
+onUnmounted(() => themeMode.destroy())
+provide('theme-mode', themeMode)
+
+</script>
+
+<template>
+    <slot></slot>
+</template>
--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: [
-                resolve(__dirname, 'src/index.vue'),
+                resolve(__dirname, 'src/index.ts'),
             ],
             formats: ['cjs', 'es']
         },
