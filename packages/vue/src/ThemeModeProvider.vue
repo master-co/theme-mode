4f918d89939e1bf0abe73100498f8c50bcce522d
Aron <i@aron.tw>
2024-03-02 15:35:22 +0800

    Add reactivity to theme mode changes in ThemeModeProvider.vue

--- packages/vue/src/ThemeModeProvider.vue
+++ packages/vue/src/ThemeModeProvider.vue
@@ -5,9 +5,18 @@ import { onMounted, provide, shallowReactive, onUnmounted } from 'vue'
 
 const { host, ...options } = withDefaults(defineProps<Options & { host?: HTMLElement }>(), defaultOptions)
 const themeMode = shallowReactive<ThemeMode>(new ThemeMode(options, host))
+// Trigger reactivity when the theme mode changes
+const onThemeModeChange = () => themeMode.value = themeMode.value
 
-onMounted(() => themeMode.init())
-onUnmounted(() => themeMode.destroy())
+onMounted(() => {
+    themeMode.init()
+    themeMode.host?.addEventListener('themeModeChange', onThemeModeChange, { passive: true })
+})
+onUnmounted(() => {
+    themeMode.destroy()
+    themeMode.host?.removeEventListener('themeModeChange', onThemeModeChange)
+})
+
 provide('theme-mode', themeMode)
 
 </script>
