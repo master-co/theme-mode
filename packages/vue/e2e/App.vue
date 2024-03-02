86ff9e64999a1319e90aa35536bd4bca3ceb83de
Aron <i@aron.tw>
2024-03-02 17:52:22 +0800

    Add Vue e2e tests for theme mode

---
+++ packages/vue/e2e/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ThemeModeProvider from '../src'
+import HelloWorld from './HelloWorld.vue';
+</script>
+
+<template>
+    <ThemeModeProvider preference="system">
+        <HelloWorld />
+    </ThemeModeProvider>
+</template>
---
+++ packages/vue/e2e/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+
+const themeMode = inject<any>('theme-mode')
+</script>
+
+<template>
+    <span id="value">{{ themeMode.value }}</span>
+    <span id="preference">{{ themeMode.preference }}</span>
+    <select class="abs full inset:0 opacity:0" v-model="themeMode.preference">
+        <option value="light">☀️ Light</option>
+        <option value="dark">🌜 Dark</option>
+        <option value="system">System</option>
+    </select>
+</template>
