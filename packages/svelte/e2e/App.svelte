5a3af24ea18fac6d07da4d486f8c8ca90df28f1a
Aron <i@aron.tw>
2024-03-03 18:37:38 +0800

    Remove unused code and update import in App.test.ts

--- packages/svelte/e2e/App.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import ThemeModeProvider from "../src/lib";
-    import HelloWorld from "./HelloWorld.svelte";
-</script>
-
-<ThemeModeProvider options={{ preference: "system" }}>
-    <HelloWorld />
-</ThemeModeProvider>
